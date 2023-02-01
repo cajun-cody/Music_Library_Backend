b0203bac98fd9673b9161c474177a9da2e087ebe
Cody <cemt5462@gmail.com>
2023-02-01 13:55:10 -0600

    Fixed issue with migration.

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -118,6 +118,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from project_name.local_settings import *
+    from music_library_project.local_settings import *
 except ImportError:
     raise ImportError("local settings not found")
