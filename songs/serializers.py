043d4cf64f8c785d570e92516be73f037667f963
Cody <cemt5462@gmail.com>
2023-02-02 11:48:19 -0600

    Created project.urls file + songs app serializer file, urls file, and views file with intital get function. + tested with brouser.

--- songs/serializers.py
+++ songs/serializers.py
@@ -1,8 +1,8 @@
-""" from rest_framework import serializers
+from rest_framework import serializers
 from .models import Song
 
-class SongSerializers(serializers.ModelSerializer):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
-        depth = 1 """
+        depth = 1
