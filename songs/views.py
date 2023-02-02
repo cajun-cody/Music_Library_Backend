63feae9ee192ac9cf7dd5e62dacb5537a9ae53b2
Cody <cemt5462@gmail.com>
2023-02-02 17:04:40 -0600

    Created Get_Song_By_Id request in views file and tested. + Updated url paths to match user story.

--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,11 @@
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .serializers import SongSerializer
 from .models import Song
-from rest_framework import status
+
+
 
 # Create your views here.
 @api_view(['GET', 'POST']) #Decorator to specify what requests can be made. 
@@ -16,3 +19,12 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True) #Statement to make sure data to POST is valid
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT','DELETE'])
+def songs_item(request, pk): #Adding pk will allow search for an object in the db by the primary key.
+    song = get_object_or_404(Song, pk=pk) #Django shortcut to quary 
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+
